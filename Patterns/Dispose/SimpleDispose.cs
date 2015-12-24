ad672a4e7b47fe231e8e1c4043df0b18bdd5ee04
prodbuild <shamajit@gmail.com>
2015-12-24 10:56:23 +0530

    afsf

---
+++ Patterns/Dispose/SimpleDispose.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics.Patterns.Dispose
+{
+    public class Customer : IDisposable
+    {
+        private bool isDisposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.isDisposed)
+            {
+                if (disposing)
+                {
+                    // free resources (managed objects) if any 
+                }
+                this.isDisposed = true;
+            }
+        }
+
+        public virtual void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~Customer()
+        {
+            Dispose(false);
+        }
+    }
+
+
+    public class GoldCustomer : Customer
+    {
+
+        private bool isDisposed = false;
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (!this.isDisposed)
+            {
+                if (disposing)
+                {
+                    // free unmanaged resources
+
+                }
+
+                this.isDisposed = true;
+            }
+
+            base.Dispose(disposing);
+        }
+
+        // The derived class does not have a Finalize method
+        // or a Dispose method without parameters because it inherits
+        // them from the base class.
+    }
+
+}
